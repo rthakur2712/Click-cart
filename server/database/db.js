e7395335aaa6341505e859badb055ac9cf1b51af
Ritik Thakur <ritik.thakur.che22@itbhu.ac.in>
2025-03-26 11:26:20 +0530

    Saved credentials and added dotenv file

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Connection = async (username, password) => {
     // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = `mongodb+srv://jaibhadrakali1234:Nm79654321@cluster0.bth1hcy.mongodb.net/`
+    const URL = `mongodb+srv://${username}:${password}@cluster0.bth1hcy.mongodb.net/`
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
